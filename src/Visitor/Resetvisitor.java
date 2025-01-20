6c1d3f3ea1ee96d60a773b0873a5d3fe468a397c
alabaer <ala_david@icloud.com>
2025-01-20 10:14:17 +0100

    added boolean reset for visitor

--- src/Visitor/Resetvisitor.java
+++ src/Visitor/Resetvisitor.java
@@ -10,21 +10,26 @@ public class Resetvisitor {
 
     public void visit(Ramp ramp) {
         ramp.setElementHitCount(0);
+        ramp.setElementStatus(false);
     }
 
    public void visit(Hole hole) {
         hole.setElementHitCount(0);
+        hole.setElementStatus(true);
     }
 
     public void visit(Target target) {
         target.setElementHitCount(0);
+        target.setElementStatus(true);
     }
 
     public void visit(Bumper bumper) {
         bumper.setElementHitCount(0);
+        bumper.setElementStatus(true);
     }
     public void visit (Slingshot slingshot){
         slingshot.setElementHitCount(0);
+        slingshot.setElementStatus(true);
     }
     public void visit (FlipperElementCompositum flipperElementCompositum){
         flipperElementCompositum.setElementHitCount(0);
